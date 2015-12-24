ce4c3bc9bc4d327ffe109560329ed30e3db37c06
awwalker <awwalker3@gmail.com>
2015-12-23 19:10:25 -0500

    erase key

--- weather.py
+++ weather.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 import sys
+import pytz
 import forecastio
 import requests
 import json
 from datetime import datetime
 
 #get api
-api_key = "3906d93478a5bec947b1c43b1f896fa8"
+api_key = "YOUR KEY"
 
 #local time
-current_time = datetime.utcnow()
-print(current_time.date())
+current_time = datetime.now(pytz.timezone("US/Eastern"))
+date = current_time.now().date()
+print(current_time)
 #retrieve location
 def get_location():
     send_url = 'http://freegeoip.net/json/'
@@ -30,5 +32,5 @@ forecast = forecastio.load_forecast(api_key, lat, lon, time = current_time)
 byHour = forecast.hourly()
 
 for data in byHour.data:
-    if data.time.utcnow().date() == current_time.date():
-        print(data.time, data.icon, data.temperature)
+    #only really want data for the day today and the hours after the current hour
+    if data.time.date() == date and data.time.time() >= current_time.now().time():
