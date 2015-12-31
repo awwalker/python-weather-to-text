a23e282225d5adf97031d4a51e6d8067f5ddaef5
awwalker <awwalker3@gmail.com>
2015-12-30 17:14:58 -0800

    fixed date issue

--- weather.py
+++ weather.py
@@ -10,22 +10,20 @@ from twilio.rest import TwilioRestClient
 """
 TO DO:
 
-cron job.
-test multiline message.
 look into icons.
-make public.
-use.
+
 """
 
 #get apis
-twil_api_account = "Your Account"
+twil_api_account = "Your account"
 twil_api_token = "Your token"
 
 twil_client = TwilioRestClient(twil_api_account, twil_api_token)
-fore_api_key = "Your key"
+fore_api_key = "Your Key"
 
 #local time adjusted with timezone
 current_time = datetime.now(pytz.timezone("US/Pacific-New"))
+
 #just date
 date = current_time.now().date()
 
@@ -43,17 +41,19 @@ coords = get_location()
 lat = coords[0]
 lon = coords[1]
 
-#initiate
-forecast = forecastio.load_forecast(fore_api_key, lat, lon, time = current_time)
+#initiate based on current time
+forecast = forecastio.load_forecast(fore_api_key, lat, lon, current_time)
 
 byHour = forecast.hourly()
+
 message = ""
 for data in byHour.data:
     #only really want data for the day today and the hours after the current hour
-    if data.time.date() == date and data.time.time() >= current_time.now().time():
-        message += data.time.strftime("%B %d, %I:%M%p") + " -- {0} at {1} degrees".format(data.summary, data.temperature) + "\n"
-        print(message)
+    if data.time.date() == date and data.time.time() >= current_time.time():
+        #just write the time, forecast and temperature for each hour
+        message += data.time.strftime("%I:%M%p") + " -- {0} at {1} degrees".format(data.summary, data.temperature) + "\n"
+
 to_me = twil_client.messages.create(
-to = "+your cell",
-from_ = "+your twilio number",
-body = "\n" + message)
+to = "+Your cell number",
+from_ = "+Your twilio number",
+body = message)
