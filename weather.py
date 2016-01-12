54dead33898c682248f35bd1b2e54c05b2079e56
awwalker <awwalker3@gmail.com>
2016-01-11 21:31:52 -0800

    final commit

--- weather.py
+++ weather.py
@@ -15,11 +15,11 @@ look into icons.
 """
 
 #get apis
-twil_api_account = "Your account"
-twil_api_token = "Your token"
+twil_api_account = "ACf73ced0c380899d61668998ee7f91c98"
+twil_api_token = "5d593fb432c3365c32634f2b55773b5a"
 
 twil_client = TwilioRestClient(twil_api_account, twil_api_token)
-fore_api_key = "Your Key"
+fore_api_key = "3906d93478a5bec947b1c43b1f896fa8"
 
 #local time adjusted with timezone
 current_time = datetime.now(pytz.timezone("US/Pacific-New"))
@@ -54,6 +54,6 @@ for data in byHour.data:
         message += data.time.strftime("%I:%M%p") + " -- {0} at {1} degrees".format(data.summary, data.temperature) + "\n"
 
 to_me = twil_client.messages.create(
-to = "+Your cell number",
-from_ = "+Your twilio number",
+to = "+14086465533",
+from_ = "+14087522765",
 body = message)
